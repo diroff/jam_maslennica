ee4a4bc35f88364ff406ffe9d881e6d9f4c0333e
scopchik <skopinevgeniy@gmail.com>
2023-05-04 18:11:38 +0300

    test darkness

--- Assets/Scripts/Darkness.cs
+++ Assets/Scripts/Darkness.cs
@@ -8,7 +8,9 @@ public class Darkness : MonoBehaviour
     [SerializeField] private Vector2Int _indexOfNode;
     [SerializeField] private PlacementTile[,] _tile;
     [SerializeField] private Node _infectedTile;
+    [SerializeField] private Node _node;
     [SerializeField] private int _countMultiply;
+    [SerializeField] private HouseBlueprint building;
     
 
 
@@ -29,12 +31,22 @@ public class Darkness : MonoBehaviour
 
     public void SetRandomDark()
     {
-        _indexOfNode.x = Random.Range(0, _indexOfNode.x + 1);
-        _indexOfNode.y = Random.Range(0, _indexOfNode.y + 1);
+        _indexOfNode.x = Random.Range(0, 29);
+        _indexOfNode.y = Random.Range(0, 29);
         _infectedTile = _house.m_Tiles[_indexOfNode.x, _indexOfNode.y].TakeNode();
         AddSquareEffect(_infectedTile, _countMultiply);
     }
 
+    public void DestroyHouse()
+    {
+        _indexOfNode.x = Random.Range(0, 29);
+        _indexOfNode.y = Random.Range(0, 29);
+        _infectedTile = _house.m_Tiles[_indexOfNode.x, _indexOfNode.y].TakeNode();
+        _infectedTile.DisableEffect();
+        
+        AddSquareEffect(_infectedTile, _countMultiply);
+        _infectedTile.Destroying(); 
+    }
 
     private void AddSquareEffect(Node startPoint, int countMultiply) //countMultiply = количество слоев квадрата
     {
